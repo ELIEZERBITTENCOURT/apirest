259a9ee9b95c520570127dd0a2a8ad0e27924dec
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-09-30 16:42:40 -0300

    Cria as classes TokenResponse e CandidateResponse com suas respectivas propriedades

---
+++ src/main/java/br/com/treinamento/apirest/api/auth/dtos/TokenResponse.java
@@ -0,0 +1,15 @@
+package br.com.treinamento.apirest.api.auth.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String token;
+
+}
---
+++ src/main/java/br/com/treinamento/apirest/api/candidates/dtos/CandidateResponse.java
@@ -0,0 +1,21 @@
+package br.com.treinamento.apirest.api.candidates.dtos;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CandidateResponse {
+    private UUID id;
+    private String name;
+    private String description;
+    private String email;
+    private String linkedin;
+    private String github;
+}
