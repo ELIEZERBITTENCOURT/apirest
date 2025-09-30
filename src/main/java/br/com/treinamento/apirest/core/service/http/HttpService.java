99c1e94572710658e6159f98f2ab7c843fbdadb4
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-09-30 16:40:28 -0300

    Remove as classes HttpService e HttpServiceImpl

--- src/main/java/br/com/treinamento/apirest/core/service/http/HttpService.java
+++
@@ -1,9 +0,0 @@
-package br.com.treinamento.apirest.core.service.http;
-
-import java.util.Optional;
-
-public interface HttpService {
-
-    <T> Optional<T> getPathVariable(String name, Class<T> type);
-    
-}
