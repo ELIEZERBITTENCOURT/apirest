0b30dbed6f6af4418f2ce3c2d0cb644396752f14
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-09-30 16:38:43 -0300

    Cria a classe LoginRequest com validações para email e senha

---
+++ src/main/java/br/com/treinamento/apirest/api/auth/dtos/LoginRequest.java
@@ -0,0 +1,23 @@
+package br.com.treinamento.apirest.api.auth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min=6)
+    private String password;
+}
