9e2519fcc8075f3b6ccdb94633c8eb17046e8436
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-09-30 16:42:16 -0300

    Cria as classes MeCandidateResponse e MeCompanyResponse com suas respectivas propriedades

---
+++ src/main/java/br/com/treinamento/apirest/api/auth/dtos/MeCompanyResponse.java
@@ -0,0 +1,22 @@
+package br.com.treinamento.apirest.api.auth.dtos;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public final class MeCompanyResponse implements MeResponse {
+    private UUID id;
+    private String name;
+    private String website;
+    private String description;
+    private String email;
+
+    @Builder.Default
+    private String role = "COMPANY";
+}
