54f35e9121ad65843f15b05bc250a26bd4f7d577
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-10-01 13:07:01 -0300

    Alteração nas classes SkillRestcontroller, SkillResponse e SkillMapper

--- src/main/java/br/com/treinamento/apirest/api/skills/mappers/SkillMapper.java
+++ src/main/java/br/com/treinamento/apirest/api/skills/mappers/SkillMapper.java
@@ -1,10 +1,8 @@
 package br.com.treinamento.apirest.api.skills.mappers;
 
 import br.com.treinamento.apirest.api.skills.dtos.SkillResponse;
-import br.com.treinamento.apirest.api.skills.dtos.SkillRequest;
 import br.com.treinamento.apirest.core.models.Skill;
 
 public interface SkillMapper{
-    Skill toSkill(SkillRequest skillRequest);
-    SkillResponse toSkillResponse(Skill skill);
+    SkillResponse toResponse(Skill skill);
 }
