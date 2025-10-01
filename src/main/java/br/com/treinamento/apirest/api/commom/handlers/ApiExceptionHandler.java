1a15344e772827340ba802f2d45da6335a28804b
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-10-01 13:09:36 -0300

    Alteração na classe Handler

--- src/main/java/br/com/treinamento/apirest/api/commom/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/treinamento/apirest/api/commom/handlers/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.treinamento.apirest.api.commom.handlers;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,39 +11,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.treinamento.apirest.api.commom.dtos.ErrorResponse;
 import br.com.treinamento.apirest.api.commom.dtos.ValidationErrorResponse;
+import br.com.treinamento.apirest.core.exceptions.ModelAlreadyExistsException;
 import br.com.treinamento.apirest.core.exceptions.ModelNotFoundException;
 
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(ModelNotFoundException.class)
+   @ExceptionHandler(ModelNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ErrorResponse handleModelNotFoundException(
-        ModelNotFoundException exception
-    ) {
-        return ErrorResponse.builder()
-            .message(exception.getLocalizedMessage())
-            .build();
+    public ErrorResponse handleModelNotFoundException(ModelNotFoundException exception) {
+        return new ErrorResponse(exception.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(ModelAlreadyExistsException.class)
+    @ResponseStatus(code = HttpStatus.CONFLICT)
+    public ErrorResponse handleModelAlreadyExistsException(ModelAlreadyExistsException exception) {
+        return new ErrorResponse(exception.getLocalizedMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ValidationErrorResponse handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException exception
-    ) {
-        var errors = exception.getBindingResult().getFieldErrors()
-            .stream()
-            .collect(Collectors.groupingBy(
-                fieldError -> fieldError.getField(),
-                Collectors.mapping(
-                    fieldError -> fieldError.getDefaultMessage(),
-                    Collectors.toList()
-                )
-            ));
+    public ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         return ValidationErrorResponse.builder()
-            .message("Validation error")
-            .errors(errors)
+            .message("one or more fields are invalid")
+            .errors(exception.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(
+                    Collectors.groupingBy(FieldError::getField,
+                    Collectors.mapping(
+                        FieldError::getDefaultMessage, Collectors.toList())
+                    )
+                )
+            )
             .build();
     }
     
